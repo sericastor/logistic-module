655fd63b2cfb7375be68c4a18556075c9888b046
jmpradillak@gmail.com <jmpradillak@gmail.com@19cee33f-03b3-ab08-b3ae-92895bb91dbb>
2011-02-27 17:20:01 +0000

    Pruebas no mas

--- trunk/src/Controller/Main.java
+++ trunk/src/Controller/Main.java
@@ -5,6 +5,10 @@
 
 package Controller;
 
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Martin Kanayet
@@ -16,7 +20,12 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Estoy funcionando y actualizando");
+        JOptionPane.showMessageDialog(null, "Hola amigos");
+        
     }
 
+
+    private javax.swing.JFrame mframe;
 }
+
+
