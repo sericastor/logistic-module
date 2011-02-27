b3e7456fd79e89f63651603e22ff7bcfef1a2c64
jmpradillak@gmail.com <jmpradillak@gmail.com@19cee33f-03b3-ab08-b3ae-92895bb91dbb>
2011-02-27 06:29:49 +0000

    src

---
+++ trunk/src/app/src/modulo_logistica_app/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modulo_logistica_app;
+
+/**
+ *
+ * @author Martin Kanayet
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Estoy funcionando amigos");
+    }
+
+}
