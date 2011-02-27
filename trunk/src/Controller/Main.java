30f2d051ee1dc22dfdbac99e9f2438f94cca00a7
edwin2207@gmail.com <edwin2207@gmail.com@19cee33f-03b3-ab08-b3ae-92895bb91dbb>
2011-02-27 23:48:11 +0000

    casiqueno

--- trunk/src/Controller/Main.java
+++ trunk/src/Controller/Main.java
@@ -18,7 +18,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JOptionPane.showMessageDialog(null, "Ahora si");
+        JOptionPane.showMessageDialog(null, "Casi que no pero por fin");
     }
 
 }
