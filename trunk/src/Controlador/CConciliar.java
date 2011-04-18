28f9e90ce8db9347f4406cb6a4d0e155ef88a64a
sericastor@gmail.com <sericastor@gmail.com@19cee33f-03b3-ab08-b3ae-92895bb91dbb>
2011-04-18 16:14:22 +0000

    ssd

--- trunk/src/Controlador/CConciliar.java
+++ trunk/src/Controlador/CConciliar.java
@@ -5,10 +5,37 @@
 
 package Controlador;
 
+import Modelo.Producto;
+import Modelo.Sistema;
+
 /**
  *
  * @author USUARIO
  */
 public class CConciliar {
-
+   public int[] numeroProductosAlmacenados(String nombre, String marca){
+       int [] cantidad = new int[5];
+       int i = 0;
+       for(Producto p: Sistema.getProductos()){
+            if(p.getNombre().equals(nombre) && p.getMarca().equals(marca)){
+                if (p.getEstado().equals("Almacenado")){
+                    cantidad[0] = p.getCantidad();
+                }
+                else if (p.getEstado().equals("Bloqueado")){
+                    cantidad[1] = p.getCantidad();
+                }
+                else if (p.getEstado().equals("En tránsito")){
+                    cantidad[2] = p.getCantidad();
+                }
+                else if (p.getEstado().equals("Perdido")){
+                    cantidad[3] = p.getCantidad();
+                }
+                else if (p.getEstado().equals("Descontinuado")){
+                    cantidad[4] = p.getCantidad();
+                }
+            }
+        }
+        return cantidad;
+    }
+   public CConciliar(){}
 }
